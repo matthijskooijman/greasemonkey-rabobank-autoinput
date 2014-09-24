ed864908110a13cbd453116f0ac9d31bdb27234a
Matthijs Kooijman <matthijs@stdin.nl>
2014-09-24 10:47:33 +0200

    Bump the version to 2.1.0 and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+Version 2.1.0 (2014-09-24)
+ - Support the Rabo Scanner pages (marginally tested).
+ - Fix bug that caused the account selection to show up on pages without
+   an account number input field.
+
 Version 2.0.0 (2014-09-15)
  - First version released by Matthijs Kooijman.
  - Make use of jquery.
--- rabobank-autoinput.user.js
+++ rabobank-autoinput.user.js
@@ -17,7 +17,7 @@
 // @copyright           2014, Matthijs Kooijman (matthijs@stdin.nl)
 // @license             The MIT license; http://opensource.org/licenses/MIT
 // @homepageURL         https://github.com/matthijskooijman/greasemonkey-rabobank-autoinput
-// @version             2.0.0
+// @version             2.1.0
 // ==/UserScript==
 
 /*jslint undef: true, vars: true, newcap: true, maxerr: 50, maxlen: 200, indent: 4 */
